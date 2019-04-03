ea7be2363db4a163b3481276a585350bb7264d90
Al Jumaily <Jumaily@users.noreply.github.com>
2019-04-03 10:55:34 -0400

    Heartbeat status of SCCM from all hosts

--- Computers Last Heartbeat Discovery.sql
+++ Computers Last Heartbeat Discovery.sql
@@ -2,3 +2,5 @@ SELECT vrs.Name0 as 'ComputerName', Vad.AgentTime as 'LastHeartBeatTime'
 FROM v_R_System as Vrs 
 INNER JOIN v_AgentDiscoveries as Vad on Vrs.ResourceID=Vad.ResourceId 
 WHERE vad.AgentName LIKE '%Heartbeat Discovery'
+
+
